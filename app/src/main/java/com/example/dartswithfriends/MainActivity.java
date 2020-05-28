495e24dc84360cc0323c0ebd4ff647db45be26bc
kmakor17 <konstantin.makor@gmail.com>
2020-05-28 15:52:24 +0200

    Main Activity 1st Commit, addPlayer finished

--- app/src/main/java/com/example/dartswithfriends/MainActivity.java
+++ app/src/main/java/com/example/dartswithfriends/MainActivity.java
@@ -3,12 +3,91 @@ package com.example.dartswithfriends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button addPlayer;
+    private Button startGame;
+    public ArrayList<Player> players = new ArrayList<>();
+    private ListView playersListView;
+    private ArrayList<String> playerNames = new ArrayList<>();
+    private ArrayAdapter<String> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addPlayer= findViewById(R.id.addPlayer_btn);
+        startGame = findViewById(R.id.startGame_btn);
+        playersListView = findViewById(R.id.players_listView);
+        players = einlesen();
+        for(Player p : players){
+            playerNames.add(p.getName());
+        }
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, playerNames);
+        playersListView.setAdapter(aa);
+        addPlayer.setOnClickListener(this);
+        startGame.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == addPlayer.getId()){
+            EditText playerName = findViewById(R.id.playerName_editText);
+            String name = playerName.getText().toString();
+            Player p = new Player(name, 0);
+            players.add(p);
+            playerNames.add(name);
+            aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, playerNames);
+        }else if(v.getId() == startGame.getId()){
+
+        }else{
+
+        }
+    }
+
+    private ArrayList<Player> einlesen(){
+        ArrayList<Player> writeBack = new ArrayList<Player>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("players.txt"));
+            String line;
+            while((line = br.readLine()) != null){
+                String[] splitted = line.split(";");
+                Player temp = new Player(splitted[0], Integer.valueOf(splitted[2]));
+                writeBack.add(temp);
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("-Eilesn- fick dich");
+            e.printStackTrace();
+        }
+        return writeBack;
+    }
+
+    private void schreiben(ArrayList<Player> p){
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter("players.txt"));
+            for(Player singleOne : p){
+                bw.write(singleOne.toString());
+            }
+            bw.flush();
+            bw.close();
+        }catch(IOException e){
+            System.out.println("-Schreiben- fick dich");
+            e.printStackTrace();
+        }
     }
 }
