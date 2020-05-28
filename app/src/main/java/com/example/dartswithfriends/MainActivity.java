d02601e363e908ffeb99adb13503b0a1f7259011
FiffiLP <fiffi.angerschmid@gmail.com>
2020-05-28 16:48:09 +0200

    Layouts überarbeitet

--- app/src/main/java/com/example/dartswithfriends/MainActivity.java
+++ app/src/main/java/com/example/dartswithfriends/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dartswithfriends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
