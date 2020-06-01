3cd30ff11e05296827110e9c8345329f9d79fdae
kmakor17 <konstantin.makor@gmail.com>
2020-06-01 14:56:59 +0200

    Start Game gefixt und es draht se

--- app/src/main/java/com/example/dartswithfriends/activities/PlayDart.java
+++ app/src/main/java/com/example/dartswithfriends/activities/PlayDart.java
@@ -26,6 +26,7 @@ public class PlayDart extends AppCompatActivity {
 
     private TableLayout screen;
 
+
     //    Preferences
     private SharedPreferences prefs;
     private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener;
