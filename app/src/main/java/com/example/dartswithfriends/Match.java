daae9e08b70424bc34b87f99542a7078044a69eb
FiffiLP <fiffi.angerschmid@gmail.com>
2020-06-01 16:28:00 +0200

    Lv Adapter basics finished

---
+++ app/src/main/java/com/example/dartswithfriends/Match.java
@@ -0,0 +1,12 @@
+package com.example.dartswithfriends;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Match {
+    HashMap<Player,Integer> game;
+
+    public Match(HashMap<Player, Integer> game) {
+        this.game = game;
+    }
+}
